ccfb8198f7081c6f36113bb1f75c46255f047138
Jkean2012 <Jkean2012@outlook.com>
2024-12-20 20:39:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://now.gg/apps/a/10004/b.html';
+const nggUrl = 'https://mathspot.com';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'https://now.gg/apps/a/10004/b.html') {
+    if (req.headers.host === 'https://mathspot.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
